53f875487007593ddbd147bf2ca745adb99367b6
Oumar220 <oumar220wansan@outlook.fr>
2026-01-07 13:44:45 +0000

    impression rdv modal

--- pages/apps/Logistique/imprimer_rapport_rdv.php
+++ pages/apps/Logistique/imprimer_rapport_rdv.php
@@ -1,5 +1,6 @@
 <?php
 $date = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
+$autoprint = isset($_GET['autoprint']) ? (int)$_GET['autoprint'] : 1;
 $pdf_url = "../impression/_rapport_rdv_jour.php?date=" . urlencode($date);
 ?>
 <!DOCTYPE html>
@@ -10,13 +11,25 @@ $pdf_url = "../impression/_rapport_rdv_jour.php?date=" . urlencode($date);
 <body style="margin:0">
     <iframe id="pdfFrame" src="<?php echo $pdf_url; ?>" style="width:100vw; height:100vh;" frameborder="0"></iframe>
     <script>
-        window.onload = function() {
-            setTimeout(function() {
+        function printPdf() {
+            try {
                 var frame = document.getElementById('pdfFrame');
-                if (frame && frame.contentWindow) {
+                if (frame && frame.contentWindow && typeof frame.contentWindow.print === 'function') {
                     frame.contentWindow.print();
+                    return true;
                 }
-            }, 1000);
+            } catch (e) {
+                // noop
+            }
+            return false;
+        }
+
+        window.onload = function() {
+            var autoprint = <?php echo (int)$autoprint; ?>;
+            if (!autoprint) return;
+            setTimeout(function() {
+                printPdf();
+            }, 700);
         };
     </script>
 </body>
--- pages/apps/secretariat/imprimer_listerdv.php
+++ pages/apps/secretariat/imprimer_listerdv.php
@@ -1,6 +1,7 @@
 <?php
 $date = isset($_GET['date']) ? $_GET['date'] : date('Y-m-d');
 $medecinId = isset($_GET['medecin']) ? (int)$_GET['medecin'] : 0;
+$autoprint = isset($_GET['autoprint']) ? (int)$_GET['autoprint'] : 1;
 $pdf_url = "../impression/_convocation_print.php?date=" . urlencode($date) . "&medecin=" . urlencode($medecinId);
 ?>
 <!DOCTYPE html>
@@ -11,10 +12,25 @@ $pdf_url = "../impression/_convocation_print.php?date=" . urlencode($date) . "&m
 <body style="margin:0">
     <iframe id="pdfFrame" src="<?php echo $pdf_url; ?>" style="width:100vw; height:100vh;" frameborder="0"></iframe>
     <script>
+        function printPdf() {
+            try {
+                var frame = document.getElementById('pdfFrame');
+                if (frame && frame.contentWindow && typeof frame.contentWindow.print === 'function') {
+                    frame.contentWindow.print();
+                    return true;
+                }
+            } catch (e) {
+                // noop
+            }
+            return false;
+        }
+
         window.onload = function() {
+            var autoprint = <?php echo (int)$autoprint; ?>;
+            if (!autoprint) return;
             setTimeout(function() {
-                document.getElementById('pdfFrame').contentWindow.print();
-            }, 1000); // attendre que le PDF charge
+                printPdf();
+            }, 700); // attendre que le PDF charge
         };
     </script>
 </body>
